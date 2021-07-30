4acfdd1df73cc4c1c903436ad96507c885b0bafa
trineshreddy <trineshreddy2000@gmail.com>
2021-07-30 12:24:35 +0530

    productlist and product details reducers are added

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,35 +1,48 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+
+import { useDispatch, useSelector } from 'react-redux'
+import Message from '../components/Message.js'
+import Loader from '../components/Loader.js'
+
 
 
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
+
 
 
 const HomeScreen = () => {
-    const [products, setProducts] = useState([])
+
+    const dispatch = useDispatch()
+
+    const productList = useSelector(state => state.productList)
+
+    const { loading, error, products } = productList
     useEffect(() => {
-        const fetchProducts = async () => {
-            const { data } = await axios.get('/api/products')
-            setProducts(data)
-        }
-        fetchProducts()
-    }, [])
+        dispatch(listProducts())
+    }, [dispatch])
     //here second argument of use Effect is an array of elments or object which when changed the useEffect has to be fired
+
 
     return (
         <>
 
             <h1>Latest Products</h1>
+            {loading ? (
+                <Loader />
+            ) : error ? (
+                <Message variant='danger'>{error}</Message>
+            ) : (
 
 
-            <Row>
-                {products.map((product) => (
-                    <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-                        <Product product={product} />
-                    </Col>
-                ))}
-            </Row>
+                <Row>
+                    {products.map((product) => (
+                        <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+                            <Product product={product} />
+                        </Col>
+                    ))}
+                </Row>)}
 
 
 
